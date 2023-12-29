d353f5cca1e7b71f38cf4140f4272c9432748c22
Grigory Serebryanyy <grishamsc@gmail.com>
2023-12-29 19:15:21 +0100

    TKUIKit
    - add TKHighlightView

---
+++ TKUIKit/Sources/TKUIKit/UIKit/Components/Views/TKHighlightView.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+public final class TKHighlightView: UIView {
+  
+  public var isHighlighted = false {
+    didSet {
+      backgroundColor = isHighlighted ? .Background.highlighted : .clear
+    }
+  }
+  
+  public override init(frame: CGRect) {
+    super.init(frame: frame)
+    backgroundColor = .clear
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
