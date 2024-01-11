5f88b7ec960dd58e84d4907af952ec011f463fad
Grigory Serebryanyy <grishamsc@gmail.com>
2024-01-11 15:28:14 +0100

    TKUIKit
    - add icons

--- TKUIKit/Sources/TKUIKit/UIKit/UIImage/UIImage+Icons.swift
+++ TKUIKit/Sources/TKUIKit/UIKit/UIImage/UIImage+Icons.swift
@@ -26,6 +26,18 @@ public extension UIImage {
           .imageWithName("Icons/28/ic-qr-viewfinder-28")
           .withRenderingMode(.alwaysTemplate)
         }
+        public static var wallet: UIImage {
+          .imageWithName("Icons/28/ic-wallet-28")
+          .withRenderingMode(.alwaysTemplate)
+        }
+        public static var purchase: UIImage {
+          .imageWithName("Icons/28/ic-purchases-28")
+          .withRenderingMode(.alwaysTemplate)
+        }
+        public static var clock: UIImage {
+          .imageWithName("Icons/28/ic-clock-28")
+          .withRenderingMode(.alwaysTemplate)
+        }
       }
       public enum Size36 {
         public static var delete: UIImage {
