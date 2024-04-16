28d5e9cf5bc767d60e009bca9e52d233ae767e90
Grigory Serebryanyy <grishamsc@gmail.com>
2024-04-16 17:16:32 +0200

    TKUIKit
    - add UINavigationController+SwipeBack
    - add BasicViewController
    - update GenericViewViewController

--- TKUIKit/Sources/TKUIKit/UIKit/UIViewController/GenericViewViewController.swift
+++ TKUIKit/Sources/TKUIKit/UIKit/UIViewController/GenericViewViewController.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-open class GenericViewViewController<View: UIView>: UIViewController {
+open class GenericViewViewController<View: UIView>: BasicViewController {
   public var customView: View { self.view as! View }
   open override func loadView() {
     view = View()
