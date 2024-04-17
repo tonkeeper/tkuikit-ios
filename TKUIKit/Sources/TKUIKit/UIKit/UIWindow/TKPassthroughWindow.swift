f36a2b8fd5ff14e92b06dd76cd18cd52cc262dfc
Grigory Serebryanyy <grishamsc@gmail.com>
2024-04-17 22:54:13 +0200

    TKUIKit
    - add ThemeMode
    - add TKWindow

--- TKUIKit/Sources/TKUIKit/UIKit/UIWindow/TKPassthroughWindow.swift
+++ TKUIKit/Sources/TKUIKit/UIKit/UIWindow/TKPassthroughWindow.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-public class TKPassthroughWindow: UIWindow {
+public class TKPassthroughWindow: TKWindow {
   public override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
     let view = super.hitTest(point, with: event)
     return view === self ? nil : view
